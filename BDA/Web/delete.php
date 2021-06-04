8d6d0a97e571f6a189279beb0fe99d9295aa4e56
Joan Ramiro <36824067+Jramiro98@users.noreply.github.com>
2021-06-04 16:43:22 +0200

    Update delete.php

--- BDA/Web/delete.php
+++ BDA/Web/delete.php
@@ -4,11 +4,8 @@ $noSQL = true;
 
 try {
 	$conn = new PDO("mysql:dbname=imdb_small;host=localhost", "root");
-	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-	#$conn->beginTransaction();
-	#$create = "CREATE TABLE IF NOT EXISTS `empresas2` (`idEmpresa` varchar(80) DEFAULT NULL,`nombreEmpresa` varchar(80) DEFAULT NULL,`accionesEmpresa` int DEFAULT NULL, `precioAcciones` int DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1"; 
-	#$conn->exec($create);
-   	#$sql = "INSERT INTO `empresas2`(`idEmpresa`,`nombreEmpresa`, `accionesEmpresa`, `precioAcciones`) VALUES ('$id','$empresa','$accions','$preu')";
+	#Parem el autocommit, així el fem manualment quan sabem que tot ha anat correctament
+	$conn->beginTransaction();
 	$sql = "DELETE FROM empresas2 WHERE idEmpresa = '$id'";
     $conn->exec($sql);
 }catch(PDOException $e){
@@ -17,10 +14,13 @@ try {
 }
 if ($noSQL){
 	try{
-			#NOSQL
+		
+		$conn->commit();
+		echo "S'ha borrat la empresa de la base de dades sql";
 
 	}catch(PDOException $e){
 	echo $sql . "<br>" . $e->getMessage();
+	#Si hem tingut algun problema fem rollback
 	$conn->rollback();
 	}
 }
