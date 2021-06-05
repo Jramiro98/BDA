3ed49557f25f9d055e3a71fa6b66261815aa1328
Joan Ramiro <36824067+Jramiro98@users.noreply.github.com>
2021-06-05 13:44:58 +0200

    Update delete.php

--- BDA/Web/delete.php
+++ BDA/Web/delete.php
@@ -14,10 +14,23 @@ try {
 }
 if ($noSQL){
 	try{
-		
+		$host = "localhost";
+		$port = 8086;
+		#inicialitzem un nou client, en host i port on tenim la base de dades.
+		$client = new InfluxDB\Client($host, $port);
+		#ens connectem a la base de dades que volem, en el nostre cas es presentació
+		$database = $client->selectDB('Empresas');
+		$points = array(new Point(
+		'Borsa', // the name of the measurement
+		null, // measurement value
+		['idEmpresa' => $id, 'nombreEmpresa' => "Empresa Borrada"], // measurement tags
+		['accionsEmpresa' => 0, 'preuAccions' => 0], // measurement fields
+		exec('date +%s%N') // timestamp in nanoseconds on Linux ONLY
+		)
+		);
+		#escribim a la base de dades
+		$result = $database->writePoints($points, Database::PRECISION_SECONDS);
 		$conn->commit();
-		echo "S'ha borrat la empresa de la base de dades sql";
-
 	}catch(PDOException $e){
 	echo $sql . "<br>" . $e->getMessage();
 	#Si hem tingut algun problema fem rollback
