4c8e46d8413867ddecca1ff7b444c66252b3099a
Joan Ramiro <36824067+Jramiro98@users.noreply.github.com>
2021-06-05 13:44:32 +0200

    Update afegir.php

--- BDA/Web/afegir.php
+++ BDA/Web/afegir.php
@@ -19,9 +19,24 @@ try {
 }
 if ($noSQL){
 	try{
-
+		$host = "localhost";
+		$port = 8086;
+		#inicialitzem un nou client, en host i port on tenim la base de dades.
+		$client = new InfluxDB\Client($host, $port);
+		#ens connectem a la base de dades que volem, en el nostre cas es presentació
+		$database = $client->selectDB('Empresas');
+		#afegim dades a partir d'una array de punts que passarem al mètode de writePOints
+		$points = array(new Point(
+		'Borsa', // the name of the measurement
+		null, // measurement value
+		['idEmpresa' => $id, 'nombreEmpresa' => $empresa], // measurement tags
+		['accionsEmpresa' => $accions, 'preuAccions' => $preu], // measurement fields
+		exec('date +%s%N') // timestamp in nanoseconds on Linux ONLY
+		)
+		);
+		#escribim a la base de dades
+		$result = $database->writePoints($points, Database::PRECISION_SECONDS);
 		$conn->commit();
-		echo "S'ha afegit la empresa a la base de dades sql";
 
 	}catch(PDOException $e){
 	echo $sql . "<br>" . $e->getMessage();
